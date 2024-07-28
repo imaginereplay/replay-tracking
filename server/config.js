183ae959c0d694ad803f4e8cb8de04e7bd4aeb0e
Luca <luca.boer@outlook.com>
2024-07-28 15:00:59 -0300

    fix: x_api_key

--- server/config.js
+++ server/config.js
@@ -1,11 +1,11 @@
-const helmet = require('@fastify/helmet');
-const compress = require('@fastify/compress');
-const cors = require('@fastify/cors');
-const fastifyCookie = require('@fastify/cookie');
-const fastifyFormbody = require('@fastify/formbody');
-const fastifyRateLimit = require('@fastify/rate-limit');
-const hpp = require('hpp');
-const sanitizeHtml = require('sanitize-html');
+const helmet = require("@fastify/helmet");
+const compress = require("@fastify/compress");
+const cors = require("@fastify/cors");
+const fastifyCookie = require("@fastify/cookie");
+const fastifyFormbody = require("@fastify/formbody");
+const fastifyRateLimit = require("@fastify/rate-limit");
+const hpp = require("hpp");
+const sanitizeHtml = require("sanitize-html");
 
 const serverConfig = (fastify) => {
   // CORS for cross-origin requests
@@ -14,7 +14,7 @@ const serverConfig = (fastify) => {
   // Rate limit plugin
   fastify.register(fastifyRateLimit, {
     max: 100,
-    timeWindow: '1 minute'
+    timeWindow: "1 minute",
   });
 
   // Cookie parser
@@ -29,12 +29,12 @@ const serverConfig = (fastify) => {
   // Compression for response
   fastify.register(compress, { global: true });
 
-  fastify.addHook('preHandler', (request, reply, done) => {
+  fastify.addHook("preHandler", (request, reply, done) => {
     hpp();
     done();
   });
 
-  fastify.addHook('preHandler', (request, reply, done) => {
+  fastify.addHook("preHandler", (request, reply, done) => {
     if (request.body) {
       request.body = sanitizeRequest(request.body);
     }
@@ -47,18 +47,22 @@ const serverConfig = (fastify) => {
     done();
   });
 
-  fastify.addHook('onRequest', async (request, reply) => {
-    const xApiKey = request.headers['xapi-key'];
-    if (process.env.NODE_ENV !== "development" && (!xApiKey || xApiKey !== process.env.X_API_KEY)) {
-      reply.code(401).send({ error: 'Unauthorized' });
+  fastify.addHook("onRequest", async (request, reply) => {
+    const xApiKey = request.headers["x_api_key"];
+    console.log("xApiKey: ", request);
+    if (
+      process.env.NODE_ENV !== "development" &&
+      (!xApiKey || xApiKey !== process.env.X_API_KEY)
+    ) {
+      reply.code(401).send({ error: "Unauthorized" });
     }
   });
 };
 
 const sanitizeRequest = (data) => {
-  if (typeof data === 'string') {
+  if (typeof data === "string") {
     return sanitizeHtml(data);
-  } else if (typeof data === 'object') {
+  } else if (typeof data === "object") {
     for (const key in data) {
       data[key] = sanitizeRequest(data[key]);
     }
